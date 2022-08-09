4704fbca4bccd84955efbeb596b505eb35fc38c8
mamadou <mamadou>
2022-08-09 20:07:55 +0100

    commit

---
+++ src/main/java/org/sun/springcloud_stream_kafka/SpringCloudStreamKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.sun.springcloud_stream_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sun/springcloud_stream_kafka/SpringCloudStreamKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sun.springcloud_stream_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
